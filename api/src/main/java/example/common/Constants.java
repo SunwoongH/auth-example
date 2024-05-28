2dfe3ee68f6085a1b9ac91c40de60895d2769cc7
sunwoong <sunwoong1122@gmail.com>
2024-05-28 18:40:46 +0900

    feat: api module constants 설정

---
+++ api/src/main/java/example/common/Constants.java
@@ -0,0 +1,7 @@
+package example.common;
+
+public abstract class Constants {
+    public static final String AUTHORIZATION = "Authorization";
+    public static final String BEARER = "Bearer ";
+    public static final String CHARACTER_TYPE = "utf-8";
+}
